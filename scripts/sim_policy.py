e998524e0b8e2a9ee43b9d6ad6897c521f45c74e
angelolovatto <angelolovatto@gmail.com>
2018-10-13 16:05:26 -0300

    Add pretty print and render option

--- scripts/sim_policy.py
+++ scripts/sim_policy.py
@@ -1,12 +1,17 @@
-import gym, torch, click, time
+import gym, torch, click, time, numpy as np, pprint
 from proj.common.saver import SnapshotSaver
 
 
 @click.command()
 @click.argument("path")
-@click.option("--index", help="Wich checkpoint to load from", type=int, default=None)
-@click.option("--runs", help="Number of episodes to simulate", type=int, default=2)
-def main(path, index, runs):
+@click.option("--index", help="Wich checkpoint to load from",
+              type=int, default=None)
+@click.option("--runs", help="Number of episodes to simulate",
+              type=int, default=2)
+@click.option("--render", help="Whether or not to render the simulation " +
+              "on screen",
+              is_flag=True)
+def main(path, index, runs, render):
     """
     Loads a snapshot and simulates the corresponding policy and environment.
     """
@@ -19,6 +24,7 @@ def main(path, index, runs):
             time.sleep(1)
         
     alg_state = state['alg_state']
+    pprint.pprint(alg_state, indent=4)
     env = alg_state['env_maker'].make(pytorch=True)
     policy = alg_state['policy']
     for _ in range(runs):
@@ -27,7 +33,7 @@ def main(path, index, runs):
         while not done:
             action = policy.action(ob)
             ob, rew, done, _ = env.step(action)
-            env.render()
+            if render: env.render()
 
     # keep unwrapping until we get the monitor
     while not isinstance(env, gym.wrappers.Monitor):
@@ -35,8 +41,12 @@ def main(path, index, runs):
             assert False
         env = env.env
     assert isinstance(env, gym.wrappers.Monitor)
-    print("Episode Rewards:", env.get_episode_rewards())
-    print("Episode Lengths:", env.get_episode_lengths())
+    if runs > 10:
+        print("Average total reward:", np.mean(env.get_episode_rewards()))
+        print("Average episode length:", np.mean(env.get_episode_lengths()))
+    else:
+        print("Episode Rewards:", env.get_episode_rewards())
+        print("Episode Lengths:", env.get_episode_lengths())
     env.unwrapped.close()
 
 
