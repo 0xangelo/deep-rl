04bb80c117454b73fae7f55b63fe8a70f364b93c
Ângelo Lovatto <angelolovatto@gmail.com>
2019-06-08 17:26:41 -0300

    refactor: use flake8 and black to refactor vanilla
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- proj/algorithms/vanilla.py
+++ proj/algorithms/vanilla.py
@@ -1,36 +1,71 @@
+"""
+Implementation of 'Vanilla' Policy Gradient in PyTorch.
+"""
 import torch
 from baselines import logger
 
 from proj.utils.tqdm_util import trange
 from proj.utils.saver import SnapshotSaver
 from proj.common.models import ValueFunction
-from proj.common.sampling import parallel_samples_collector, compute_pg_vars, \
-    flatten_trajs
+from proj.common.sampling import (
+    parallel_samples_collector,
+    compute_pg_vars,
+    flatten_trajs,
+)
 from proj.common.env_makers import VecEnvMaker
 import proj.common.log_utils as logu
 
 
-def vanilla(env, policy, val_fn=None, total_steps=int(1e6), steps=125,
-            n_envs=16, gamma=0.99, gaelam=0.97, optimizer={}, val_iters=80,
-            val_lr=1e-3, **saver_kwargs):
+TOTAL_STEPS_DEFAULT = int(1e6)
 
-    optimizer = {'class': torch.optim.Adam, **optimizer}
-    if val_fn is None:
-        val_fn = ValueFunction.from_policy(policy)
+
+def vanilla(
+    env,
+    policy,
+    val_fn=None,
+    total_steps=TOTAL_STEPS_DEFAULT,
+    steps=125,
+    n_envs=16,
+    gamma=0.99,
+    gaelam=0.97,
+    optimizer=None,
+    val_iters=80,
+    val_lr=1e-3,
+    **saver_kwargs
+):
+    """
+    Vanilla Policy Gradient
+
+    env: instance of proj.common.env_makers.VecEnvMaker
+    policy: instance of proj.common.models.Policy
+    val_fn (optional): instance of proj.common.models.ValueFunction
+    total_steps: total number of environment steps to take
+    steps: number of steps to take in each environment per iteration
+    n_envs: number of environment copies to run in parallel
+    gamma: GAE discount parameter
+    gaelam: GAE lambda exponential average parameter
+    optimizer (optional): dictionary containing optimizer kwargs and/or class
+    val_iters: number of optimization steps to update the critic per iteration
+    val_lr: learning rate for critic optimizer
+    saver_kwargs: keyword arguments for proj.utils.saver.SnapshotSaver
+    """
+    optimizer = optimizer or {}
+    optimizer = {"class": torch.optim.Adam, **optimizer}
+    val_fn = val_fn or ValueFunction.from_policy(policy)
 
     logu.save_config(locals())
     saver = SnapshotSaver(logger.get_dir(), locals(), **saver_kwargs)
 
     vec_env = VecEnvMaker(env)(n_envs)
-    policy = policy.pop('class')(vec_env, **policy)
-    val_fn = val_fn.pop('class')(vec_env, **val_fn)
-    pol_optim = optimizer.pop('class')(policy.parameters(), **optimizer)
+    policy = policy.pop("class")(vec_env, **policy)
+    val_fn = val_fn.pop("class")(vec_env, **val_fn)
+    pol_optim = optimizer.pop("class")(policy.parameters(), **optimizer)
     val_optim = torch.optim.Adam(val_fn.parameters(), lr=val_lr)
     loss_fn = torch.nn.MSELoss()
 
     # Algorithm main loop
     collector = parallel_samples_collector(vec_env, policy, steps)
-    beg, end, stp = steps * n_envs, total_steps + steps*n_envs, steps * n_envs
+    beg, end, stp = steps * n_envs, total_steps + steps * n_envs, steps * n_envs
     for samples in trange(beg, end, stp, desc="Training", unit="step"):
         logger.info("Starting iteration {}".format(samples // stp))
         logger.logkv("Iteration", samples // stp)
@@ -39,7 +74,7 @@ def vanilla(env, policy, val_fn=None, total_steps=int(1e6), steps=125,
         trajs = next(collector)
 
         logger.info("Computing policy gradient variables")
-        compute_pg_vars(trajs, policy, val_fn, gamma, gaelam)
+        compute_pg_vars(trajs, val_fn, gamma, gaelam)
         flatten_trajs(trajs)
         all_obs, all_acts, _, _, all_advs, all_vals, all_rets = trajs.values()
         all_obs, all_vals = all_obs[:-n_envs], all_vals[:-n_envs]
@@ -47,9 +82,9 @@ def vanilla(env, policy, val_fn=None, total_steps=int(1e6), steps=125,
         logger.info("Applying policy gradient")
         all_dists = policy(all_obs)
         old_dists = all_dists.detach()
-        J0 = torch.mean(all_dists.log_prob(all_acts) * all_advs)
+        objective = torch.mean(all_dists.log_prob(all_acts) * all_advs)
         pol_optim.zero_grad()
-        (-J0).backward()
+        objective.neg().backward()
         pol_optim.step()
 
         logger.info("Updating val_fn")
@@ -59,8 +94,8 @@ def vanilla(env, policy, val_fn=None, total_steps=int(1e6), steps=125,
             val_optim.step()
 
         logger.info("Logging information")
-        logger.logkv("Objective", J0.item())
-        logger.logkv('TotalNSamples', samples)
+        logger.logkv("Objective", objective.item())
+        logger.logkv("TotalNSamples", samples)
         logu.log_reward_statistics(vec_env)
         logu.log_val_fn_statistics(all_vals, all_rets)
         logu.log_action_distribution_statistics(old_dists)
@@ -76,7 +111,7 @@ def vanilla(env, policy, val_fn=None, total_steps=int(1e6), steps=125,
                 val_fn=val_fn.state_dict(),
                 pol_optim=pol_optim.state_dict(),
                 val_optim=val_optim.state_dict(),
-            )
+            ),
         )
         del all_obs, all_acts, all_advs, all_vals, all_rets, trajs
 
