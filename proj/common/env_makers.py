920f17689cd95efb83ad1d13836313cdcf4154f4
Ângelo Lovatto <angelolovatto@gmail.com>
2019-06-08 19:43:44 -0300

    refactor: make env_makers and log_utils flake8 and black compliant
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- proj/common/env_makers.py
+++ proj/common/env_makers.py
@@ -1,3 +1,6 @@
+"""
+Implements several factories for both single and vectorized environments.
+"""
 import os
 import gym
 import numpy as np
@@ -9,20 +12,27 @@ from baselines.common.vec_env.vec_monitor import VecMonitor
 from proj.common.env_pool import EnvPool, ShmEnvPool
 
 
-class EnvMaker(object):
+class EnvMaker:
+    """
+    Used to store an environment id and apply the appropriate wrappers
+    when constructing the environment.
+    """
+
     def __init__(self, env_id):
         self.env_id = env_id
         self.__name__ = repr(self)
 
     def __call__(self):
-        if 'AtariEnv' in gym.spec(self.env_id)._entry_point \
-           and '-ram-' not in self.env_id:
+        if (
+            "AtariEnv" in gym.spec(self.env_id)._entry_point
+            and "-ram-" not in self.env_id
+        ):
             env = make_atari(self.env_id)
             env = wrap_deepmind(env)
         else:
             env = gym.make(self.env_id)
 
-        if len(env.observation_space.shape) == 1 and 'TimeLimit' in str(env):
+        if len(env.observation_space.shape) == 1 and "TimeLimit" in str(env):
             env = AddRelativeTimestep(env)
 
         return env
@@ -31,7 +41,12 @@ class EnvMaker(object):
         return "EnvMaker('{}')".format(self.env_id)
 
 
-class VecEnvMaker(object):
+class VecEnvMaker:
+    """
+    Used to store an environment id and apply the appropriate vectorized
+    environment wrappers when constructing the vectorized environment.
+    """
+
     def __init__(self, env_id):
         self.env_id = env_id
         self.__name__ = repr(self)
@@ -39,8 +54,10 @@ class VecEnvMaker(object):
     def __call__(self, n_envs=1, *, train=True):
         env_fn = EnvMaker(self.env_id)
 
-        if 'AtariEnv' in gym.spec(self.env_id)._entry_point \
-           and '-ram-' not in self.env_id:
+        if (
+            "AtariEnv" in gym.spec(self.env_id)._entry_point
+            and "-ram-" not in self.env_id
+        ):
             if n_envs == 1:
                 vec_env = DummyVecEnv([env_fn])
             else:
@@ -53,10 +70,10 @@ class VecEnvMaker(object):
                 vec_env = EnvPool(env_fn, n_envs=n_envs)
 
         monitor_dir = os.path.join(
-            logger.get_dir(), ('train' if train else 'eval') + "_monitor")
+            logger.get_dir(), ("train" if train else "eval") + "_monitor"
+        )
         os.makedirs(monitor_dir, exist_ok=True)
         vec_env = VecMonitor(vec_env, filename=monitor_dir)
-        setattr(vec_env, 'directory', os.path.abspath(monitor_dir))
         return vec_env
 
     def __repr__(self):
@@ -67,43 +84,64 @@ class VecEnvMaker(object):
 # Reproducible DummyVecEnv
 # ==============================
 
+
 class DummyVecEnv(_DummyVecEnv):
+    """
+    Extends baselines.common.vec_env.dummy_vec_env.DummyVecEnv to allow seeding
+    and perform it on initialization.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # set initial seeds
         seeds = np.random.randint(
-            low=0, high=np.iinfo(np.int32).max, size=self.num_envs)
+            low=0, high=np.iinfo(np.int32).max, size=self.num_envs
+        )
         self.seed(seeds)
 
     def seed(self, seeds):
         for env, seed in zip(self.envs, seeds):
             env.seed(int(seed))
 
+
 # ==============================
 # Wrappers
 # ==============================
 
+
 class AddTimestep(gym.ObservationWrapper):
+    """
+    Adds the absolute timestep to the observations of environments with
+    observation spaces of type gym.spaces.Box.
+    """
+
     def __init__(self, env=None):
         super().__init__(env)
         self.observation_space = gym.spaces.Box(
             low=np.append(self.observation_space.low, 0),
-            high=np.append(self.observation_space.high, 2**32),
-            dtype=self.observation_space.dtype)
+            high=np.append(self.observation_space.high, 2 ** 32),
+            dtype=self.observation_space.dtype,
+        )
 
     def observation(self, observation):
         return np.append(observation, self.env._elapsed_steps)
 
 
 class AddRelativeTimestep(gym.ObservationWrapper):
+    """
+    Adds the relative timestep (normalized to the range [-1, 1]) to the
+    observations of environments with observation spaces of type gym.spaces.Box.
+    """
+
     def __init__(self, env=None):
         super().__init__(env)
         self.observation_space = gym.spaces.Box(
-            low=np.append(self.observation_space.low, -1.),
-            high=np.append(self.observation_space.high, 1.),
-            dtype=self.observation_space.dtype)
+            low=np.append(self.observation_space.low, -1.0),
+            high=np.append(self.observation_space.high, 1.0),
+            dtype=self.observation_space.dtype,
+        )
 
     def observation(self, observation):
         return np.append(
-            observation,
-            -1 + (self.env._elapsed_steps / self.spec.timestep_limit)*2)
+            observation, -1 + (self.env._elapsed_steps / self.spec.timestep_limit) * 2
+        )
