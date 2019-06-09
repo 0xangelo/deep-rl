705cd3ded8a5d0696e2815c08353b0778d948b60
Ângelo Lovatto <angelolovatto@gmail.com>
2019-06-09 19:08:08 -0300

    refactor: make viskit flake8 and black compliant
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- proj/utils/viskit/core.py
+++ proj/utils/viskit/core.py
@@ -22,14 +22,10 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
-
 import itertools
 import pandas
 import json
 import os
-
-import numpy as np
 
 
 class AttrDict(dict):
@@ -50,11 +46,11 @@ def load_progress(progress_path, verbose=True):
     if verbose:
         print("Reading %s" % progress_path)
 
-    if progress_path.endswith('.csv'):
-        return pandas.read_csv(progress_path, index_col=None, comment='#')
+    if progress_path.endswith(".csv"):
+        return pandas.read_csv(progress_path, index_col=None, comment="#")
 
     ds = []
-    with open(progress_path, 'rt') as fh:
+    with open(progress_path, "rt") as fh:
         for line in fh:
             ds.append(json.loads(line))
     return pandas.DataFrame(ds)
@@ -73,7 +69,7 @@ def flatten_dict(d):
 
 
 def load_params(params_json_path):
-    with open(params_json_path, 'r') as f:
+    with open(params_json_path, "r") as f:
         data = json.loads(f.read())
         if "args_data" in data:
             del data["args_data"]
@@ -121,11 +117,11 @@ def load_exps_data(exp_folder_paths, ignore_missing_keys=False, verbose=True):
                 params = load_params(variant_json_path)
             else:
                 params = dict(exp_name="experiment")
-            exps_data.append(AttrDict(
-                progress=progress,
-                params=params,
-                flat_params=flatten_dict(params)
-            ))
+            exps_data.append(
+                AttrDict(
+                    progress=progress, params=params, flat_params=flatten_dict(params)
+                )
+            )
         except (IOError, pandas.errors.EmptyDataError) as e:
             if verbose:
                 print(e)
@@ -160,16 +156,17 @@ def smart_repr(x):
         else:
             return "(" + ",".join(map(smart_repr, x)) + ")"
     else:
-        if hasattr(x, "__call__"):
-            return "__import__('pydoc').locate('%s')" % \
-                (x.__module__ + "." + x.__name__)
+        if callable(x):
+            return "__import__('pydoc').locate('%s')" % (
+                x.__module__ + "." + x.__name__
+            )
         else:
             return repr(x)
 
 
-def extract_distinct_params(exps_data,
-                            excluded_params=('exp_name', 'seed', 'log_dir'),
-                            l=1):
+def extract_distinct_params(
+    exps_data, excluded_params=("exp_name", "seed", "log_dir"), minimum=1
+):
     try:
         stringified_pairs = sorted(
             map(
@@ -177,31 +174,29 @@ def extract_distinct_params(exps_data,
                 unique(
                     flatten(
                         [
-                            list(
-                                map(
-                                    smart_repr,
-                                    list(d.flat_params.items())
-                                )
-                            )
+                            list(map(smart_repr, list(d.flat_params.items())))
                             for d in exps_data
                         ]
                     )
-                )
+                ),
             ),
-            key=lambda x: (
-                tuple("" if it is None else str(it) for it in x),
-            )
+            key=lambda x: (tuple("" if it is None else str(it) for it in x),),
         )
     except Exception as e:
         print(e)
         import ipdb
+
         ipdb.set_trace()
     proposals = [
         (k, [x[1] for x in v])
         for k, v in itertools.groupby(stringified_pairs, lambda x: x[0])
     ]
-    filtered = [(k, v) for (k, v) in proposals if len(v) > l and all(
-        [k.find(excluded_param) != 0 for excluded_param in excluded_params])]
+    filtered = [
+        (k, v)
+        for (k, v) in proposals
+        if len(v) > minimum
+        and all([k.find(excluded_param) != 0 for excluded_param in excluded_params])
+    ]
     return filtered
 
 
@@ -219,17 +214,22 @@ class Selector(object):
 
     def where(self, k, v):
         return Selector(
-            self._exps_data, self._filters + ((k, v),), self._custom_filters)
+            self._exps_data, self._filters + ((k, v),), self._custom_filters
+        )
 
     def custom_filter(self, filter):
-        return Selector(
-            self._exps_data, self._filters, self._custom_filters + [filter])
+        return Selector(self._exps_data, self._filters, self._custom_filters + [filter])
 
     def _check_exp(self, exp):
         # or exp.flat_params.get(k, None) is None
         return all(
-            ((str(exp.flat_params.get(k, None)) == str(v) or (
-                k not in exp.flat_params)) for k, v in self._filters)
+            (
+                (
+                    str(exp.flat_params.get(k, None)) == str(v)
+                    or (k not in exp.flat_params)
+                )
+                for k, v in self._filters
+            )
         ) and all(custom_filter(exp) for custom_filter in self._custom_filters)
 
     def extract(self):
@@ -241,22 +241,23 @@ class Selector(object):
 
 # Taken from plot.ly
 color_defaults = [
-    '#1f77b4',  # muted blue
-    '#ff7f0e',  # safety orange
-    '#2ca02c',  # cooked asparagus green
-    '#d62728',  # brick red
-    '#9467bd',  # muted purple
-    '#8c564b',  # chestnut brown
-    '#e377c2',  # raspberry yogurt pink
-    '#7f7f7f',  # middle gray
-    '#bcbd22',  # curry yellow-green
-    '#17becf'  # blue-teal
+    "#1f77b4",  # muted blue
+    "#ff7f0e",  # safety orange
+    "#2ca02c",  # cooked asparagus green
+    "#d62728",  # brick red
+    "#9467bd",  # muted purple
+    "#8c564b",  # chestnut brown
+    "#e377c2",  # raspberry yogurt pink
+    "#7f7f7f",  # middle gray
+    "#bcbd22",  # curry yellow-green
+    "#17becf",  # blue-teal
 ]
 
 
 def hex_to_rgb(hex, opacity=1.0):
-    if hex[0] == '#':
+    if hex[0] == "#":
         hex = hex[1:]
-    assert (len(hex) == 6)
+    assert len(hex) == 6
     return "rgba({0},{1},{2},{3})".format(
-        int(hex[:2], 16), int(hex[2:4], 16), int(hex[4:6], 16), opacity)
+        int(hex[:2], 16), int(hex[2:4], 16), int(hex[4:6], 16), opacity
+    )
--- proj/utils/viskit/frontend.py
+++ proj/utils/viskit/frontend.py
@@ -22,10 +22,7 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
-
 import os
-
 import flask
 from proj.utils.viskit import core
 import sys
@@ -34,6 +31,7 @@ import json
 import numpy as np
 import plotly.offline as po
 import plotly.graph_objs as go
+
 
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
@@ -53,8 +51,9 @@ def sliding_mean(data_array, window=5):
     data_array = np.array(data_array)
     new_list = []
     for i in range(len(data_array)):
-        indices = list(range(max(i - window + 1, 0),
-                             min(i + window + 1, len(data_array))))
+        indices = list(
+            range(max(i - window + 1, 0), min(i + window + 1, len(data_array)))
+        )
         avg = 0
         for j in indices:
             avg += data_array[j]
@@ -64,10 +63,7 @@ def sliding_mean(data_array, window=5):
     return np.array(new_list)
 
 
-app = flask.Flask(
-    __name__,
-    static_url_path='/static',
-)
+app = flask.Flask(__name__, static_url_path="/static")
 
 exps_data = None
 plottable_keys = None
@@ -76,14 +72,14 @@ distinct_params = None
 data_paths = None
 
 
-@app.route('/js/<path:path>')
+@app.route("/js/<path:path>")
 def send_js(path):
-    return flask.send_from_directory('js', path)
+    return flask.send_from_directory("js", path)
 
 
-@app.route('/css/<path:path>')
+@app.route("/css/<path:path>")
 def send_css(path):
-    return flask.send_from_directory('css', path)
+    return flask.send_from_directory("css", path)
 
 
 def make_plot(plot_list, title=None, xtitle=None, ytitle=None):
@@ -105,53 +101,51 @@ def make_plot(plot_list, title=None, xtitle=None, ytitle=None):
                 y_lower = list(plt.means - plt.ses)
             else:
                 raise NotImplementedError
-            data.append(go.Scatter(
-                x=x + x[::-1],
-                y=y_upper + y_lower[::-1],
-                fill='tozerox',
-                fillcolor=core.hex_to_rgb(color, 0.2),
-                line=go.scatter.Line(color='rgba(0,0,0,0)'),
-                showlegend=False,
-                legendgroup=plt.legend,
-                hoverinfo='none'
-            ))
-            data.append(go.Scatter(
-                x=x,
-                y=y,
-                name=plt.legend,
-                legendgroup=plt.legend,
-                line=dict(color=core.hex_to_rgb(color)),
-            ))
-        elif plt.display_mode == "individual":
-            for idx, y in enumerate(plt.ys):
-                data.append(go.Scatter(
+            data.append(
+                go.Scatter(
+                    x=x + x[::-1],
+                    y=y_upper + y_lower[::-1],
+                    fill="tozerox",
+                    fillcolor=core.hex_to_rgb(color, 0.2),
+                    line=go.scatter.Line(color="rgba(0,0,0,0)"),
+                    showlegend=False,
+                    legendgroup=plt.legend,
+                    hoverinfo="none",
+                )
+            )
+            data.append(
+                go.Scatter(
                     x=x,
                     y=y,
                     name=plt.legend,
                     legendgroup=plt.legend,
                     line=dict(color=core.hex_to_rgb(color)),
-                    showlegend=idx == 0,
-                ))
+                )
+            )
+        elif plt.display_mode == "individual":
+            for idx, y in enumerate(plt.ys):
+                data.append(
+                    go.Scatter(
+                        x=x,
+                        y=y,
+                        name=plt.legend,
+                        legendgroup=plt.legend,
+                        line=dict(color=core.hex_to_rgb(color)),
+                        showlegend=idx == 0,
+                    )
+                )
         else:
             raise NotImplementedError
 
     layout = go.Layout(
-        legend=dict(
-            x=1,
-            y=1,
-            borderwidth=1,
-        ),
+        legend=dict(x=1, y=1, borderwidth=1),
         title=title,
         titlefont=dict(size=20),
-        margin=go.layout.Margin(
-            t=35,
-            b=40,
-            r=30,
-        ),
+        margin=go.layout.Margin(t=35, b=40, r=30),
         xaxis=go.layout.XAxis(
-            range=[xmin,xmax],
+            range=[xmin, xmax],
             showline=True,
-            mirror='ticks',
+            mirror="ticks",
             title=xtitle,
             gridwidth=2,
             tickfont=dict(size=14),
@@ -159,21 +153,22 @@ def make_plot(plot_list, title=None, xtitle=None, ytitle=None):
         ),
         yaxis=go.layout.YAxis(
             showline=True,
-            mirror='ticks',
+            mirror="ticks",
             title=ytitle,
             gridwidth=2,
             tickfont=dict(size=14),
             titlefont=dict(size=16),
-        )
+        ),
     )
     fig = go.Figure(data=data, layout=layout)
-    fig_div = po.plot(fig, output_type='div', include_plotlyjs=True)
+    fig_div = po.plot(fig, output_type="div", include_plotlyjs=True)
     if "footnote" in plot_list[0]:
-        footnote = "<br />".join([
-            r"<span><b>%s</b></span>: <span>%s</span>" % (
-                plt.legend, plt.footnote)
-            for plt in plot_list
-        ])
+        footnote = "<br />".join(
+            [
+                r"<span><b>%s</b></span>: <span>%s</span>" % (plt.legend, plt.footnote)
+                for plt in plot_list
+            ]
+        )
         return r"%s<div>%s</div>" % (fig_div, footnote)
     else:
         return fig_div
@@ -184,17 +179,11 @@ def summary_name(exp, selector=None):
 
 
 def check_nan(exp):
-    return all(
-        not np.any(np.isnan(vals)) for vals in list(exp.progress.values()))
+    return all(not np.any(np.isnan(vals)) for vals in list(exp.progress.values()))
 
 
 def get_plot_instruction(
-        x_plot_key,
-        plot_key,
-        display_mode,
-        split_key=None,
-        group_key=None,
-        filters=None,
+    x_plot_key, plot_key, display_mode, split_key=None, group_key=None, filters=None
 ):
     selector = core.Selector(exps_data)
     if filters is None:
@@ -212,18 +201,18 @@ def get_plot_instruction(
     plots = []
     counter = 0
     for split_selector, split_legend in zip(split_selectors, split_legends):
-        if group_key and group_key is not "exp_name":
+        if group_key and group_key != "exp_name":
             vs = [vs for k, vs in distinct_params if k == group_key][0]
             group_selectors = [split_selector.where(group_key, v) for v in vs]
             group_legends = [str(x) for x in vs]
         else:
             group_key = "exp_name"
             # Separate data in groups according to experiment name
-            vs = set([x.params["exp_name"]
-                         for x in split_selector.extract()])
+            vs = set([x.params["exp_name"] for x in split_selector.extract()])
             group_selectors = [split_selector.where(group_key, v) for v in vs]
-            group_legends = [summary_name(
-                x.extract()[0], split_selector) for x in group_selectors]
+            group_legends = [
+                summary_name(x.extract()[0], split_selector) for x in group_selectors
+            ]
 
         to_plot = []
         for group_selector, group_legend in zip(group_selectors, group_legends):
@@ -254,23 +243,28 @@ def get_plot_instruction(
                 else:
                     # first decide what the xs should be
                     # ideally, it should be the union of
-                    all_xs = np.unique(np.sort(np.concatenate(
-                        [d.progress.get(x_plot_key, [])
-                         for d in filtered_data])))
+                    all_xs = np.unique(
+                        np.sort(
+                            np.concatenate(
+                                [d.progress.get(x_plot_key, []) for d in filtered_data]
+                            )
+                        )
+                    )
 
                     interpolated_progresses = []
                     for d in filtered_data:
                         if x_plot_key in d.progress:
                             x_to_interp = d.progress[x_plot_key]
                             y_to_interp = d.progress.get(
-                                plot_key, np.ones(len(x_to_interp)) * np.nan)
+                                plot_key, np.ones(len(x_to_interp)) * np.nan
+                            )
                             interpolated_progresses.append(
                                 np.interp(
                                     all_xs,
                                     x_to_interp,
                                     y_to_interp,
                                     left=np.nan,
-                                    right=np.nan
+                                    right=np.nan,
                                 )
                             )
                         else:
@@ -283,41 +277,48 @@ def get_plot_instruction(
                 if display_mode == "mean_std":
                     means = np.atleast_1d(np.nanmean(progresses, axis=0))
                     stds = np.atleast_1d(np.nanstd(progresses, axis=0))
-                    to_plot.append(AttrDict(
-                        means=means,
-                        stds=stds,
-                        legend=group_legend,
-                        xs=xs,
-                        display_mode=display_mode,
-                    ))
+                    to_plot.append(
+                        AttrDict(
+                            means=means,
+                            stds=stds,
+                            legend=group_legend,
+                            xs=xs,
+                            display_mode=display_mode,
+                        )
+                    )
                 elif display_mode == "mean_se":
                     means = np.atleast_1d(np.nanmean(progresses, axis=0))
-                    ses = np.atleast_1d(np.nanstd(progresses, axis=0) / \
-                        np.sqrt(np.sum(1 - np.isnan(progresses), axis=0)))
-                    to_plot.append(AttrDict(
-                        means=means,
-                        ses=ses,
-                        legend=group_legend,
-                        xs=xs,
-                        display_mode=display_mode,
-                    ))
+                    ses = np.atleast_1d(
+                        np.nanstd(progresses, axis=0)
+                        / np.sqrt(np.sum(1 - np.isnan(progresses), axis=0))
+                    )
+                    to_plot.append(
+                        AttrDict(
+                            means=means,
+                            ses=ses,
+                            legend=group_legend,
+                            xs=xs,
+                            display_mode=display_mode,
+                        )
+                    )
                 elif display_mode == "individual":
-                    to_plot.append(AttrDict(
-                        xs=xs,
-                        ys=progresses,
-                        legend=group_legend,
-                        display_mode=display_mode,
-                    ))
+                    to_plot.append(
+                        AttrDict(
+                            xs=xs,
+                            ys=progresses,
+                            legend=group_legend,
+                            display_mode=display_mode,
+                        )
+                    )
                 else:
                     raise NotImplementedError
 
         if len(to_plot) > 0:
-            plots.append(make_plot(
-                to_plot,
-                title=split_legend,
-                xtitle=x_plot_key,
-                ytitle=plot_key,
-            ))
+            plots.append(
+                make_plot(
+                    to_plot, title=split_legend, xtitle=x_plot_key, ytitle=plot_key
+                )
+            )
 
         counter += 1
     return "\n".join(plots)
@@ -347,17 +348,13 @@ def plot_div():
     if len(group_key) == 0:
         group_key = None
 
-    # print(reload_s3, type(reload_s3))
-
     if reload_s3:
-        project_root = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), ".."))
+        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
         print(data_paths)
         for data_path in data_paths:
             if "data/s3/" in data_path:
                 exp_group = data_path.split("data/s3/")[-1].split("/")[0]
-                os.system("python %s/scripts/sync_s3.py %s" %
-                          (project_root, exp_group))
+                os.system("python %s/scripts/sync_s3.py %s" % (project_root, exp_group))
         reload_data()
 
     plot_div = get_plot_instruction(
@@ -385,8 +382,12 @@ def index():
         group_key = None
     print("Getting plot instruction...")
     plot_div = get_plot_instruction(
-        x_plot_key="(default)", plot_key=plot_key, display_mode="mean_std",
-        split_key=None, group_key=group_key)
+        x_plot_key="(default)",
+        plot_key=plot_key,
+        display_mode="mean_std",
+        split_key=None,
+        group_key=group_key,
+    )
     print("Rendering...")
     rendered = flask.render_template(
         "main.html",
@@ -397,8 +398,7 @@ def index():
         x_plot_key="(default)",
         x_plottable_keys=["(default)"] + x_plottable_keys,
         distinct_param_keys=[str(k) for k, v in distinct_params],
-        distinct_params=dict([(str(k), list(map(str, v)))
-                              for k, v in distinct_params]),
+        distinct_params=dict([(str(k), list(map(str, v))) for k, v in distinct_params]),
     )
     return rendered
 
@@ -412,8 +412,9 @@ def is_increasing_key(key, exps_data):
 
 def is_increasing(arr):
     arr = np.asarray(arr)
-    return np.all(np.nansum([arr[1:],-arr[:-1]], axis=0) >= 0) \
-        and np.nanmax(arr) >= np.nanmin(arr)
+    return np.all(np.nansum([arr[1:], -arr[:-1]], axis=0) >= 0) and np.nanmax(
+        arr
+    ) >= np.nanmin(arr)
 
 
 def reload_data(verbose=False):
@@ -422,17 +423,19 @@ def reload_data(verbose=False):
     global distinct_params
     global x_plottable_keys
     exps_data = core.load_exps_data(data_paths, verbose=verbose)
-    plottable_keys = sorted(list(
-        set(flatten(list(exp.progress.keys()) for exp in exps_data))))
+    plottable_keys = sorted(
+        list(set(flatten(list(exp.progress.keys()) for exp in exps_data)))
+    )
     distinct_params = sorted(core.extract_distinct_params(exps_data))
     x_plottable_keys = [
-        key for key in plottable_keys if is_increasing_key(key, exps_data)]
+        key for key in plottable_keys if is_increasing_key(key, exps_data)
+    ]
 
 
 if __name__ == "__main__":
     default_port = int(os.environ.get("VISKIT_PORT", 5000))
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_paths", type=str, nargs='*')
+    parser.add_argument("data_paths", type=str, nargs="*")
     parser.add_argument("--debug", action="store_true", default=False)
     parser.add_argument("--port", type=int, default=default_port)
     parser.add_argument("-v", "--verbose", action="store_true", default=False)
@@ -445,4 +448,4 @@ if __name__ == "__main__":
     url = "http://localhost:%d" % (args.port)
     print("Done! View %s in your browser" % (url))
 
-    app.run(host='0.0.0.0', port=args.port, debug=args.debug, threaded=True)
+    app.run(host="0.0.0.0", port=args.port, debug=args.debug, threaded=True)
