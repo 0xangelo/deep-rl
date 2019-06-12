132aa29297bda27c459c1dbca494c3a5bfaa9a1d
Ângelo Lovatto <angelolovatto@gmail.com>
2019-06-12 17:20:28 -0300

    refactor: use contextlib redirect_stdout in tqdm_util
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- proj/utils/tqdm_util.py
+++ proj/utils/tqdm_util.py
@@ -1,57 +1,43 @@
+""" Utility functions to handle tqdm output"""
 import sys
 import contextlib
 from tqdm import tqdm as _tqdm, trange as _trange
 
 
-ORIG_STD_OUT_ERR = (sys.stdout, sys.stderr)
-
-
-class DummyTqdmFile(object):
+class DummyTqdmFile:
     """
     Dummy file-like that will write to tqdm
     """
 
-    file = None
+    file = sys.stdout
 
     def __init__(self, file):
         self.file = file
 
-    def write(self, x):
+    def write(self, output):
         # Avoid print() second call (useless \n)
-        if len(x.rstrip()) > 0:
-            _tqdm.write(x, file=self.file)
+        if output.rstrip():
+            _tqdm.write(output, file=self.file)
 
-    def read(self, x):
+    @staticmethod
+    def read(_):
         raise RuntimeError("Can't read from DummyTqdmFile")
 
     def flush(self):
         return getattr(self.file, "flush", lambda: None)()
 
 
-def std_out():
-    return ORIG_STD_OUT_ERR[0]
-
-
-@contextlib.contextmanager
 def tqdm_out():
-    try:
-        sys.stdout, sys.stderr = map(DummyTqdmFile, ORIG_STD_OUT_ERR)
-        yield
-    # Relay exceptions
-    except Exception as exc:
-        raise exc
-    # Always restore sys.stdout/err if necessary
-    finally:
-        sys.stdout, sys.stderr = ORIG_STD_OUT_ERR
+    return contextlib.redirect_stdout(DummyTqdmFile(sys.stdout))
 
 
 def trange(*args, **kwargs):
     """
-    A wrapper for tqdm.trange, automatically setting file=std_out() and
+    A wrapper for tqdm.trange, automatically setting file=sys.stdout and
     dynamic_ncols=True if not specified.
     """
     if "file" not in kwargs:
-        kwargs["file"] = std_out()
+        kwargs["file"] = DummyTqdmFile.file
     if "dynamic_ncols" not in kwargs:
         kwargs["dynamic_ncols"] = True
     return _trange(*args, **kwargs)
@@ -59,11 +45,11 @@ def trange(*args, **kwargs):
 
 def tqdm(*args, **kwargs):
     """
-    A wrapper for tqdm.tqdm, automatically setting file=std_out() and
+    A wrapper for tqdm.tqdm, automatically setting file=sys.stdout and
     dynamic_ncols=True if not specified.
     """
     if "file" not in kwargs:
-        kwargs["file"] = std_out()
+        kwargs["file"] = DummyTqdmFile.file
     if "dynamic_ncols" not in kwargs:
         kwargs["dynamic_ncols"] = True
     return _tqdm(*args, **kwargs)
