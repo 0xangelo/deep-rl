d1edd0517976cf4fcad86993854c15058b9baf3a
Ângelo Lovatto <angelolovatto@gmail.com>
2019-06-16 10:44:55 -0300

    chore: standardize setup.py
    Make `setup.py` similar to the one suggested in the PyPi packaging
    tutorial: `https://packaging.python.org/tutorials/packaging-projects/`
    
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='proj',
-      packages=[package for package in find_packages()
-                if package.startswith('proj')],
-      install_requires=[
-          'gym',
-          'numpy',
-          'tqdm',
-          'flask',
-          'plotly',
-          'click',
-          'cloudpickle',
-          'torch'
-      ],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deep-rl-angelolovatto",
+    version="0.1.0",
+    author="Ângelo Gregório Lovatto",
+    author_email="angelolovatto@gmail.com",
+    description="PyTorch implementation of reinforcement learning algorithms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/angelolovatto/deep-rl",
+    packages=[
+        package for package in setuptools.find_packages() if package.startswith("proj")
+    ],
+    install_requires=[
+        "gym",
+        "numpy",
+        "tqdm",
+        "flask",
+        "plotly",
+        "click",
+        "cloudpickle",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
