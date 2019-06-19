325461018a8774247f7cf2cf8efa5571685e1248
Ângelo Lovatto <angelolovatto@gmail.com>
2019-06-19 13:38:44 -0300

    chore: update proj inititialization
    OpenAI baselines automatically creates a temporary directory
    when the OPENAI_LOGDIR env variable is not set.
    
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- proj/__init__.py
+++ proj/__init__.py
@@ -1,15 +1,11 @@
-import os
-import tempfile
 from gym.envs.registration import register
-os.environ['OPENAI_LOGDIR'] = os.path.join(tempfile.gettempdir(), 'deep-rl')
-
 
 # === CartPole-v1 modifications ===
 for stddev in (0.0, 0.5, 1.0, 1.5):
     register(
-        id='Gauss{}CartPole-v1'.format(stddev),
-        entry_point='proj.envs:RandomCartPoleEnv',
-        kwargs={'noise_scale': stddev},
+        id="Gauss{}CartPole-v1".format(stddev),
+        entry_point="proj.envs:RandomCartPoleEnv",
+        kwargs={"noise_scale": stddev},
         max_episode_steps=500,
         reward_threshold=475.0,
     )
